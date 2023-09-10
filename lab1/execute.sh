bf5b4030bc3ab10598a6e3cc0c7c17b1370f6fa7
Nate Foster <jnfoster@cs.cornell.edu>
2023-09-10 12:06:23 -0400

    ANSI colors FTW

--- lab1/execute.sh
+++ lab1/execute.sh
@@ -12,8 +12,8 @@ cp $@ mystery.stf
     mystery.p4
 if [ $? -eq 0 ]
 then
-   echo "[\033[0;32m\033[0;1mPassed\033[0m]"
+    echo -e "\033[32m\033[1mPassed\033[0m"
 else
-   echo "[\033[0;31m\033[0;1mFailed\033[0m]"    
+    echo -e "\033[31m\033[1mFailed\033[0m"
 fi
 rm mystery.stf
